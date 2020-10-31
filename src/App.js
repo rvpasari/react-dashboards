e778ca5b1aa924abf5c0fc968d726ebad569aa8b
rvpasari <rvpasari@gmail.com>
2020-10-31 11:48:39 +0530

    Basic fusionchart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react"; 
+import ReactFC from 'react-fusioncharts'; 
+import FusionCharts from 'fusioncharts';
+import Column2D from 'fusioncharts/fusioncharts.charts';
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+const chartData = [
+  {
+    label: "Venezuela",
+    value: "290"
+  },
+  {
+    label: "Saudi",
+    value: "260"
+  },
+  {
+    label: "Canada",
+    value: "180"
+  },
+  {
+    label: "Iran",
+    value: "140"
+  },
+  {
+    label: "Russia",
+    value: "115"
+  },
+  {
+    label: "UAE",
+    value: "100"
+  },
+  {
+    label: "US",
+    value: "30"
+  },
+  {
+    label: "China",
+    value: "30"
+  }
+];
+
+
+const chartConfigs = { 
+
+  type: "column2d", 
+  width: "700", 
+  height: '400',
+  dataFormat: "json",
+  dataSource: { 
+    chart: { 
+      caption: "Countries with the most oil reserves", 
+      subCaption: "In MMbbl = One Million barrels", 
+      xAxisName: "Country", 
+      yAxisName: "Reserves (MMbbl)", 
+      numberSuffix: "K", 
+      theme: "fusion"
+    },
+    data: chartData
+  }
+};
+
+class App extends React.Component { 
+  render() { 
+    return (<ReactFC {...chartConfigs}/>);
+  }
 }
 
 export default App;
+
